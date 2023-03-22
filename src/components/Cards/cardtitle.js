f844d4f6f8d3c3f7a2ea4751ec07997ffd563446
Leandro Lemos <leandroolemoss17@gmail.com>
2023-03-22 06:40:39 -0300

    New page feature

---
+++ src/components/Cards/cardtitle.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './cards.css';
+
+export default function title(){
+    return (
+        
+        <div className="Card-title">
+                <h4>Nome</h4>
+                <h4>Telefone</h4>
+                <h4>Email</h4>
+                <h4>Profissao</h4>
+                <h4>Idade</h4>
+            </div>
+        
+    );
+}
